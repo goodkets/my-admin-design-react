773b89d771e8696458f012596e048a4dfa74ea54
goodkat <2066442690@qq.com>
2024-07-31 15:55:39 +0800

    项目代码检查配置以及格式化配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteESLintPlugin from 'vite-plugin-eslint'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteESLintPlugin({
+      // 开发阶段因为 ESLint 的错误, 不再会打断开发
+      failOnError: false
+    })
+  ]
 })
